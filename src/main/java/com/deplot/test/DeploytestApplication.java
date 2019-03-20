8a2fc3344e3d964f0ca7974f1f3775b22b083ab7
ms.seol <ms.seol@igsinc.co.kr>
2019-03-20 10:08:50 +0900

    hello

---
+++ src/main/java/com/deplot/test/DeploytestApplication.java
@@ -0,0 +1,13 @@
+package com.deplot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploytestApplication.class, args);
+	}
+
+}
